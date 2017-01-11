35101c5f42e08308cbb483cc612ba20e84881df4
James Fountain <jamesfountain6@gmail.com>
2017-01-11 20:31:58 +0000

    Added messy implementation of get current running app stuff overlollipop
    Needs a lot of work, currently only logs all running apps sorted by last time opened

--- app/src/main/java/com/example/anton/sticky_notes_1test/BackgroundService.java
+++ app/src/main/java/com/example/anton/sticky_notes_1test/BackgroundService.java
@@ -1,17 +1,28 @@
 package com.example.anton.sticky_notes_1test;
 
+import android.annotation.TargetApi;
 import android.app.ActivityManager;
 import android.app.Service;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.lang.reflect.Field;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Anton on 30-Nov-16.
@@ -35,7 +46,7 @@ public class BackgroundService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        Toast.makeText(this, "Background process strated", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Background process started", Toast.LENGTH_LONG).show();
 
         t.scheduleAtFixedRate(new TimerTask() {
             @Override
@@ -68,33 +79,115 @@ public class BackgroundService extends Service {
 
     private String currentApp = null;
 
+    @TargetApi(android.os.Build.VERSION_CODES.LOLLIPOP)
     public String getCurrentAppOverLollipop()
     {
-        final int PROCESS_STATE_TOP = 2;
-        ActivityManager.RunningAppProcessInfo currentInfo = null;
-        Field field = null;
-        try {
-            field = ActivityManager.RunningAppProcessInfo.class.getDeclaredField("processState");
-        } catch (Exception ignored) {
-        }
-        ActivityManager am = (ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.RunningAppProcessInfo> appList = am.getRunningAppProcesses();
-        for (ActivityManager.RunningAppProcessInfo app : appList) {
-            if (app.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND
-                    && app.importanceReasonCode == ActivityManager.RunningAppProcessInfo.REASON_UNKNOWN) {
-                Integer state = null;
-                try {
-                    state = field.getInt(app);
-                } catch (Exception e) {
-                }
-                if (state != null && state == PROCESS_STATE_TOP) {
-                    currentInfo = app;
-                    break;
-                }
-            }
-        }
-        return currentInfo.toString();
+        printUsageStats(getUsageStatsList(this));
+        getStats();
+        return getUsageStatsList(this).toString();
     }
+
+    @TargetApi(23)
+    private void getStats()
+    {
+        UsageStatsManager lUsageStatsManager = (UsageStatsManager) getSystemService(Context.USAGE_STATS_SERVICE);
+        List<UsageStats> lUsageStatsList = lUsageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, System.currentTimeMillis()- TimeUnit.DAYS.toMillis(1),System.currentTimeMillis()+ TimeUnit.DAYS.toMillis(1));
+
+        //TODO change this to be readable, ugly AF atm
+//        TextView lTextView = (TextView) findViewById(R.id.someTextThing);
+// Cant access Layout Views from service???
+
+        //TODO Use a map, then sort by the lastTimeUsed
+        HashMap<String, Long> appList = new HashMap<String, Long>();
+
+        StringBuilder lStringBuilder = new StringBuilder();
+
+        for (UsageStats lUsageStats:lUsageStatsList)
+        {
+            appList.put(lUsageStats.getPackageName(), lUsageStats.getLastTimeUsed());
+
+//            lStringBuilder.append(lUsageStats.getPackageName());
+//            lStringBuilder.append(" - ");
+//            lStringBuilder.append(lUsageStats.getLastTimeUsed());
+//            lStringBuilder.append("\r\n");
+        }
+
+
+
+        List<Map.Entry<String, Long>> list = new LinkedList<>( appList.entrySet() );
+
+        Collections.sort(list, new Comparator<Map.Entry<String, Long>>() {
+            @Override
+            public int compare(Map.Entry<String, Long> o1, Map.Entry<String, Long> o2) {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        Map<String, Long> result = new LinkedHashMap<>();
+
+        for (Map.Entry<String, Long> entry : list)
+        {
+            result.put( entry.getKey(), entry.getValue() );
+        }
+
+        // Just checking whats in here
+        int i = result.size();
+
+
+        for (String name:result.keySet())
+        {
+            StringBuilder resultString = new StringBuilder();
+
+            resultString.append(name.toString());
+            resultString.append(" - ");
+            resultString.append(result.get(name).toString());
+            resultString.append("\r\n");
+        }
+        //TODO get the most recent one from the list!
+
+        //Prints running apps sorted by lasttime used
+        for (String name: result.keySet()){
+            String key = name.toString();
+            String value = result.get(name).toString();
+            Log.d(TAG, key + " " + value);
+        }
+//        lTextView.setText(lStringBuilder.toString());
+    }
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("M-d-yyyy HH:mm:ss");
+    public static final String TAG = BackgroundService.class.getSimpleName();
+
+    @TargetApi(android.os.Build.VERSION_CODES.LOLLIPOP)
+    public static List<UsageStats> getUsageStatsList(Context context){
+        UsageStatsManager usm = getUsageStatsManager(context);
+        Calendar calendar = Calendar.getInstance();
+        long endTime = calendar.getTimeInMillis();
+        calendar.add(Calendar.YEAR, -1);
+        long startTime = calendar.getTimeInMillis();
+
+        Log.d(TAG, "Range start:" + dateFormat.format(startTime) );
+        Log.d(TAG, "Range end:" + dateFormat.format(endTime));
+
+        List<UsageStats> usageStatsList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, startTime, endTime);
+        return usageStatsList;
+    }
+    @TargetApi(android.os.Build.VERSION_CODES.LOLLIPOP)
+    public static void printUsageStats(List<UsageStats> usageStatsList){
+        for (UsageStats u : usageStatsList)
+        {
+            Log.d(TAG, "Pkg: " + u.getPackageName() +  "\t" + "ForegroundTime: "
+                    + u.getTotalTimeInForeground()) ;
+        }
+    }
+
+    @SuppressWarnings("ResourceType")
+    private static UsageStatsManager getUsageStatsManager(Context context){
+        UsageStatsManager usm = (UsageStatsManager) context.getSystemService("usagestats");
+        return usm;
+    }
+
+
+
 
     public void getCurrentAppUnderLollipop()
     {
--- app/src/main/java/com/example/anton/sticky_notes_1test/MainActivity.java
+++ app/src/main/java/com/example/anton/sticky_notes_1test/MainActivity.java
@@ -1,45 +1,49 @@
 package com.example.anton.sticky_notes_1test;
 
-import android.app.Activity;
-import android.app.ActivityManager;
+import android.annotation.TargetApi;
+import android.app.AppOpsManager;
 import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Iterator;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.SortedMap;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.TreeMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS = 100;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,10 +52,13 @@ public class MainActivity extends AppCompatActivity {
         });
 
         final Button buttonStart = (Button) findViewById(R.id.acttionbutton);
+
         buttonStart.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                getRunningActivities();
-//                getCurrentApp();
+                //Checks permission, if not available, requests permission
+                //May need to repeat after getting permission!
+                fillStats();
+
                 createBackgroundService();
             }
         });
@@ -64,16 +71,94 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void gotoSecondPage() {
+    private void fillStats()
+    {
+        if (hasPermission())
+        {
+            getStats();
+        }
+        else
+        {
+            requestPermission();
+        }
+    }
+
+    private void requestPermission()
+    {
+        Toast.makeText(this, "Need to request permission", Toast.LENGTH_SHORT).show();
+        startActivityForResult(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS), MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS);
+    }
+
+    @TargetApi(23)
+    private void getStats()
+    {
+        UsageStatsManager lUsageStatsManager = (UsageStatsManager) getSystemService(Context.USAGE_STATS_SERVICE);
+        List<UsageStats> lUsageStatsList = lUsageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, System.currentTimeMillis()- TimeUnit.DAYS.toMillis(1),System.currentTimeMillis()+ TimeUnit.DAYS.toMillis(1));
+
+        //TODO change this to be readable, ugly AF atm
+        TextView lTextView = (TextView) findViewById(R.id.someTextThing);
+
+        //TODO Use a map, then sort by the lastTimeUsed
+        HashMap<String, Long> appList = new HashMap<String, Long>();
+
+        StringBuilder lStringBuilder = new StringBuilder();
+
+        for (UsageStats lUsageStats:lUsageStatsList)
+        {
+            appList.put(lUsageStats.getPackageName(), lUsageStats.getLastTimeUsed());
+
+//            lStringBuilder.append(lUsageStats.getPackageName());
+//            lStringBuilder.append(" - ");
+//            lStringBuilder.append(lUsageStats.getLastTimeUsed());
+//            lStringBuilder.append("\r\n");
+        }
+
+
+
+        List<Map.Entry<String, Long>> list = new LinkedList<>( appList.entrySet() );
+
+        Collections.sort(list, new Comparator<Map.Entry<String, Long>>() {
+            @Override
+            public int compare(Map.Entry<String, Long> o1, Map.Entry<String, Long> o2) {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        Map<String, Long> result = new LinkedHashMap<>();
+
+        for (Map.Entry<String, Long> entry : list)
+        {
+            result.put( entry.getKey(), entry.getValue() );
+        }
+
+        // Just checking whats in here
+        int i = result.size();
+
+
+        lTextView.setText(lStringBuilder.toString());
+    }
+
+    private boolean hasPermission()
+    {
+        AppOpsManager appOps = (AppOpsManager) getSystemService(Context.APP_OPS_SERVICE);
+        int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
+                android.os.Process.myUid(), getPackageName());
+        return mode == AppOpsManager.MODE_ALLOWED;
+    }
+
+    private void gotoSecondPage()
+    {
         Intent secondPage = new Intent(this, SecondScreen.class);
         startActivity(secondPage);
     }
 
-    private void createBackgroundService() {
+    private void createBackgroundService()
+    {
         startService(new Intent(getBaseContext(), BackgroundService.class));
     }
 
-    private void stopBackgroudService(){
+    private void stopBackgroudService()
+    {
         String stoping = "Stopping background process";
         String stopped = "Background process is stopped";
 
@@ -82,77 +167,25 @@ public class MainActivity extends AppCompatActivity {
         Log.e("Log: ", stopped);
     }
 
-    private void getRunningActivities() {
-
-        TextView content = (TextView)findViewById(R.id.content);
-        content.setMovementMethod(new ScrollingMovementMethod());
-
-        ActivityManager am = (ActivityManager) getSystemService(Activity.ACTIVITY_SERVICE);
-        List<ActivityManager.RunningAppProcessInfo> runningAppProccessInfo = am.getRunningAppProcesses();
-
-        for (int i=0; i<runningAppProccessInfo.size(); i++){
-            content.append(runningAppProccessInfo.get(i).processName + "\n");
-        }
-
-    }
-
-    private void getCurrentApp(){
-        TextView current_app = (TextView)findViewById(R.id.current_app);
-
-        String currentApp;
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            // intentionally using string value as Context.USAGE_STATS_SERVICE was
-            // strangely only added in API 22 (LOLLIPOP_MR1)
-            @SuppressWarnings("WrongConstant")
-            UsageStatsManager usm = (UsageStatsManager) getSystemService("usagestats");
-            long time = System.currentTimeMillis();
-            List<UsageStats> appList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,
-                    time - 1000 * 1000, time);
-            if (appList != null && appList.size() > 0) {
-                SortedMap<Long, UsageStats> mySortedMap = new TreeMap<Long, UsageStats>();
-                for (UsageStats usageStats : appList) {
-                    mySortedMap.put(usageStats.getLastTimeUsed(),
-                            usageStats);
-                }
-                if (mySortedMap != null && !mySortedMap.isEmpty()) {
-                    currentApp = mySortedMap.get(mySortedMap.lastKey()).getPackageName();
-                    current_app.append("\n\t" + currentApp);
-                    Log.e("Current app >= Lollipop",currentApp);
-                }
-            }
-        } else {
-            //this method is getting the package name of the first process which is not
-            //always the currently oped application
-            ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-            List<ActivityManager.RunningAppProcessInfo> tasks = am.getRunningAppProcesses();
-            currentApp = tasks.get(0).processName;
-
-            //this is showing only our application name. It might work if
-            //we run the application in background - not tested yet
-            //currentApp = getApplicationContext().getPackageName();
-
-            current_app.append("\n\t" + currentApp);
-            Log.e("Current app <= Lollipop",currentApp);
-        }
-    }
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
